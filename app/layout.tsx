073c08833c590f33d6e29743bc4760e25a199a73
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-06-23 22:33:53 +0900

    i18n setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,21 @@ import "./globals.css";
 import { inter } from "@/components/common/fonts";
 import NextThemeProvider from "./theme-provider";
 import logo from "@/public/icons/logo.svg";
+import { getLocale } from "next-intl/server";
 
 export const metadata = {
   title: "JIHYEON JEONG",
   description: "frontend dev",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const locale = await getLocale();
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={inter.className}>
         <NextThemeProvider defaultTheme="dark">{children}</NextThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Post from "@/components/middleSide/post";
 import Project from "@/components/middleSide/project";
 
 import * as delays from "@/constants/delays";
+import { useTranslations } from "next-intl";
 import dynamic from "next/dynamic";
 
 export const metadata = {
@@ -22,6 +23,8 @@ const DynamicLeftSide = dynamic(() => import("@/components/leftSide"));
 const DynamicRightSide = dynamic(() => import("@/components/rightSide"));
 
 export default function Home() {
+  const t = useTranslations("HomePage");
+  console.log(t("title"));
   return (
     <main className="scroll-smooth bg-hover-color-light w-full h-screen dark:bg-body-color text-text-grey overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-text-dark/20 scrollbar-thumb-text-dark/60">
       <DynamicNavBar />
