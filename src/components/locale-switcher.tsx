0b2a7cfde6ca9f23aeff7d90c3f2134a2843e278
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-07-24 14:51:52 +0900

    project 6

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,30 +1,38 @@
-import { Locale, useLocale } from 'next-intl';
-import { RiEnglishInput } from 'react-icons/ri';
-import { TbAlphabetKorean } from 'react-icons/tb';
-import { usePathname, useRouter } from '@/i18n/navigation';
-import { useParams } from 'next/navigation';
+import { Locale, useLocale } from "next-intl";
+import { RiEnglishInput } from "react-icons/ri";
+import { TbAlphabetKorean } from "react-icons/tb";
+import { usePathname, useRouter } from "@/i18n/navigation";
+import { useParams } from "next/navigation";
+import { useTransition } from "react";
 
 const LocaleSwitcher = () => {
   const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
   const params = useParams();
+  const [isPending, startTransition] = useTransition();
 
   const onClickLocale = () => {
-    const nextLocale: Locale = locale === 'en' ? 'ko' : 'en'; // 현재 로케일에 따라 다음 로케일 결정
-
-    router.replace(
-      // @ts-expect-error -- TypeScript will validate that only known `params`
-      // are used in combination with a given `pathname`. Since the two will
-      // always match for the current route, we can skip runtime checks.
-      { pathname, params },
-      { locale: nextLocale }
-    );
+    const nextLocale: Locale = locale === "en" ? "ko" : "en"; // 현재 로케일에 따라 다음 로케일 결정
+    startTransition(async () => {
+      router.replace(
+        // @ts-expect-error -- TypeScript will validate that only known `params`
+        // are used in combination with a given `pathname`. Since the two will
+        // always match for the current route, we can skip runtime checks.
+        { pathname, params },
+        { locale: nextLocale }
+      );
+    });
   };
 
   return (
-    <button aria-label="switch-locale" type="button" onClick={onClickLocale}>
-      {locale === 'en' ? (
+    <button
+      disabled={isPending}
+      aria-label="switch-locale"
+      type="button"
+      onClick={onClickLocale}
+    >
+      {locale === "en" ? (
         <RiEnglishInput size={25} />
       ) : (
         <TbAlphabetKorean size={25} />
